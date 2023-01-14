1108b170cae6139d38fd5480602a7de46320e71e
Your Name <you@example.com>
2023-01-14 17:05:36 -0500

    todo edit

--- server.js
+++ server.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+ *  WEB422 – Assignment 1
+ *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.
+ *  No part of this assignment has been copied manually or electronically from any other source
+ *  (including web sites) or distributed to other students.
+ *
+ *  Name: Nishant Sharma     Student ID: 150619203    Date: 14/01/2023
+ *  Cyclic Link: _______________________________________________________________
+ *
+ ********************************************************************************/
+
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
