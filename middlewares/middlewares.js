474611cf2df3109be87c238083c930d70c4154ec
Your Name <you@example.com>
2023-01-14 15:46:36 -0500

    rearranging

---
+++ middlewares/middlewares.js
@@ -0,0 +1,20 @@
+// Error Handlers middlewares
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalURL}`);
+  res.status(404);
+  next(error);
+};
+
+const errorHandler = (error, req, res, next) => {
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+  res.json({
+    message: error.message,
+    stack: error.stack,
+  });
+};
+
+module.exports = {
+  notFound,
+  errorHandler,
+};
--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+
+const middlewares = require("./middlewares/middlewares.js");
 const MoviesDB = require("./modules/moviesDB.js");
+const routes = require("./api/routes.js");
 
 const app = express();
 
@@ -18,6 +21,13 @@ const port = process.env.PORT;
 app.get("/", (req, res) => {
   res.json({ message: "API Listening" });
 });
+
+// api routes
+app.get("/api/movies", routes);
+
+// Error Handlers, see ./middlewares/middlewares.js
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
 
 db.initialize(process.env.MONGODB_CONN_STRING)
   .then(() => {
